21826ebf53d8543b138dd340c88ac67058a8ca5c
Dariusz Działak <darek@dzialak.pl>
2023-06-12 09:48:58 +0200

    checkpoint-successfully-retruns-sms-es

---
+++ codes.py
@@ -0,0 +1,96 @@
+GSM7_Table = [
+    "000A", "000C", "000D", "0020", "0021", "0022", "0023", "0024", "0025", "0026", "0027", "0028",
+    "0029", "002A", "002B", "002C", "002D", "002E", "002F", "0030", "0031", "0032", "0033", "0034", "0035", "0036",
+    "0037", "0038", "0039", "003A", "003A", "003B", "003C", "003D", "003E", "003F", "0040", "0041", "0042", "0043",
+    "0044", "0045", "0046", "0047", "0048", "0049", "004A", "004B", "004C", "004D", "004E", "004F", "0050", "0051",
+    "0052", "0053", "0054", "0055", "0056", "0057", "0058", "0059", "005A", "005B", "005C", "005D", "005E", "005F",
+    "0061", "0062", "0063", "0064", "0065", "0066", "0067", "0068", "0069", "006A", "006B", "006C", "006D", "006E",
+    "006F", "0070", "0071", "0072", "0073", "0074", "0075", "0076", "0077", "0078", "0079", "007A", "007B", "007C",
+    "007D", "007E", "00A0", "00A1", "00A3", "00A4", "00A5", "00A7", "00BF", "00C4", "00C5", "00C6", "00C7", "00C9",
+    "00D1", "00D6", "00D8", "00DC", "00DF", "00E0", "00E4", "00E5", "00E6", "00E8", "00E9", "00EC", "00F1", "00F2",
+    "00F6", "00F8", "00F9", "00FC", "0393", "0394", "0398", "039B", "039E", "03A0", "03A3", "03A6", "03A8", "03A9",
+    "20AC"
+]
+
+GSM7_Table_Extend = ["007B", "007D", "005B", "005D", "007E", "005C", "005E", "20AC", "007C"]
+
+
+def hex2char(hex_value):
+    result = ''
+    n = int(hex_value, 16)
+    if n <= 0xFFFF:
+        result += chr(n)
+    elif n <= 0x10FFFF:
+        n -= 0x10000
+        result += chr(0xD800 | (n >> 10)) + chr(0xDC00 | (n & 0x3FF))
+    return result
+
+
+def dec2hex(text_string):
+    return hex(int(text_string))[2:].upper()
+
+
+def get_encode_type(str_message):
+    encode_type = "GSM7_default"
+    gsm7_extend_char_len = 0
+
+    if not str_message:
+        return {"encodeType": encode_type, "extendLen": gsm7_extend_char_len}
+
+    for char in str_message:
+        char_code = format(ord(char), "04X")
+        if char_code in GSM7_Table_Extend:
+            gsm7_extend_char_len += 1
+        if char_code not in GSM7_Table:
+            encode_type = "UNICODE"
+            gsm7_extend_char_len = 0
+            break
+
+    return {"encodeType": encode_type, "extendLen": gsm7_extend_char_len}
+
+def encode_message(text_string):
+    haut = 0
+    result = ''
+
+    if not text_string:
+        return result
+
+    for char in text_string:
+        b = ord(char)
+        if haut != 0:
+            if 0xDC00 <= b <= 0xDFFF:
+                result += dec2hex(0x10000 + ((haut - 0xD800) << 10) + (b - 0xDC00))
+                haut = 0
+                continue
+            else:
+                haut = 0
+
+        if 0xD800 <= b <= 0xDBFF:
+            haut = b
+        else:
+            cp = dec2hex(b)
+            cp = cp.zfill(4)
+            result += cp
+
+    return result
+
+
+specialChars = ['000D', '000A', '0009', '0000']
+specialCharsIgnoreWrap = ['0009', '0000']
+
+
+def decode_message(string, ignore_wrap):
+    if not string:
+        return ""
+
+    specials = specialCharsIgnoreWrap if ignore_wrap else specialChars
+
+    def replace(match):
+        parens = match.group(1)
+        if parens not in specials:
+            return hex2char(parens)
+        else:
+            return ''
+
+    import re
+    return re.sub(r'([A-Fa-f0-9]{1,4})', replace, string)
--- send_sms.py
+++ send_sms.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
 
 
-import sys
 import base64
 import codecs
+import logging
+import subprocess
+import sys
 import time
+
 import requests
 
-from encodings import normalize_encoding
+from codes import encode_message
+from codes import decode_message
 
-import subprocess
+logging.basicConfig(level=logging.DEBUG)
+
+logger = logging.getLogger(__name__)
 
 ROUTER_IP = '192.168.0.1'
 URL = None
@@ -24,7 +30,7 @@ _cache = {}
 def setup_router():
     global ROUTER_IP, URL, HEADERS
 
-    code, gateway_ips = subprocess.getstatusoutput("ip r | sed -n 's/default via \\([^ ]*\\) .*/\\1/p'")
+    code, gateway_ips = subprocess.getstatusoutput("ip r | sed -n 's/default via \\([^ ]*\\) .*/\\1/p' | tail -n 1")
     if code == 0:
         lines = gateway_ips.splitlines()
         if lines and lines[0]:
@@ -37,129 +43,14 @@ def setup_router():
                               f'Referer': f'http://{ROUTER_IP}/index.html'}
 
 
-def decode_gsm7(txt, errors):
-    ext_table = {
-        '\x40': u'|',
-        '\x14': u'^',
-        '\x65': u'€',
-        '\x28': u'{',
-        '\x29': u'}',
-        '\x3C': u'[',
-        '\x3D': u'~',
-        '\x3E': u']',
-        '\x2F': u'\\',
-    }
-    chunks = filter(None, txt.split('\x1b'))  # split on ESC
-    res = u''
-    for chunk in chunks:
-        res += ext_table[chunk[0]]  # first character after ESC
-        if len(chunk) > 1:
-            # charmap_decode returns a tuple..
-            decoded, _ = codecs.charmap_decode(chunk[1:], errors, decoding_table)
-            res += decoded
-    return res, len(txt)
-
-
-decoding_table = (
-    u"@£$¥èéùìòÇ\nØø\rÅå" +
-    u"Δ_ΦΓΛΩΠΨΣΘΞ\x1bÆæßÉ" +
-    u" !\"#¤%&'()*+,-./" +
-    u"0123456789:;<=>?" +
-    u"¡ABCDEFGHIJKLMNO" +
-    u"PQRSTUVWXYZÄÖÑÜ§" +
-    u"¿abcdefghijklmno" +
-    u"pqrstuvwxyzäöñüà"
-)
-
-encoding_table = codecs.charmap_build(
-    decoding_table + '\0' * (256 - len(decoding_table))
-)
-
-# extending the encoding table with extension characters
-encoding_table[ord(u'|')] = '\x1b\x40'
-encoding_table[ord(u'^')] = '\x1b\x14'
-encoding_table[ord(u'€')] = '\x1b\x65'
-encoding_table[ord(u'{')] = '\x1b\x28'
-encoding_table[ord(u'}')] = '\x1b\x29'
-encoding_table[ord(u'[')] = '\x1b\x3C'
-encoding_table[ord(u'~')] = '\x1b\x3D'
-encoding_table[ord(u']')] = '\x1b\x3E'
-encoding_table[ord(u'\\')] = '\x1b\x2F'
-
-
-class GSM7Codec(codecs.Codec):
-
-    def encode(self, txt, errors='strict'):
-        return codecs.charmap_encode(txt, errors, encoding_table)
-
-    def decode(self, txt, errors='strict'):
-        return decode_gsm7(txt, errors)
-
-
-class GSM7IncrementalEncoder(codecs.IncrementalEncoder):
-    def encode(self, input, final=False):
-        return codecs.charmap_encode(input, self.errors, encoding_table)[0]
-
-
-class GSM7IncrementalDecoder(codecs.IncrementalDecoder):
-    def decode(self, input, final=False):
-        return codecs.charmap_decode(input, self.errors, decoding_table)[0]
-
-
-class GSM7StreamWriter(codecs.StreamWriter):
-    pass
-
-
-class GSM7StreamReader(codecs.StreamReader):
-    pass
-
-
-def search_function(encoding):
-    """Register the gsm-7 encoding with Python's codecs API. This involves
-       adding a search function that takes in an encoding name, and returns
-       a codec for that encoding if it knows one, or None if it doesn't.
-    """
-    if encoding in _cache:
-        return _cache[encoding]
-    norm_encoding = normalize_encoding(encoding)
-    if norm_encoding in ('gsm_7', 'g7', 'gsm7'):
-        cinfo = codecs.CodecInfo(
-            name='gsm-7',
-            encode=GSM7Codec().encode,
-            decode=GSM7Codec().decode,
-            incrementalencoder=GSM7IncrementalEncoder,
-            incrementaldecoder=GSM7IncrementalDecoder,
-            streamreader=GSM7StreamReader,
-            streamwriter=GSM7StreamWriter,
-        )
-        _cache[norm_encoding] = cinfo
-        return cinfo
-
-
-codecs.register(search_function)
-
-
-def two_dig(c):
-    res = hex(c)[2:].upper()
-    if len(res) == 1:
-        return '0' + res
-    return res
-
-
 def encode_sms_body(data, codec):
-    buf = []
-    for c in data:
-        chars = c.encode(codec)
-        if len(chars) > 1:
-            buf.append(two_dig(chars[-1]))
-            buf.append(two_dig(chars[-2]))
-        else:
-            buf.append('00')
-            buf.append(two_dig(chars[-1]))
-
-    result = ''.join(buf)
+    result = encode_message(data)
     print("[%s] Converted %s -> %s" % (codec, data, result))
     return result
+
+
+def decode_sms_body(data):
+    return decode_message(data, True)
 
 
 def is_phone_number(ph):
@@ -168,10 +59,115 @@ def is_phone_number(ph):
 
 def hilfe():
     print("Usage: %s phone_number message" % sys.argv[0])
+    print("Usage: %s check [ts]" % sys.argv[0])
     sys.exit(2)
 
 
+def login():
+    setup_router()
+    print(f"Login {URL}")
+    passwd = base64.encodebytes(PASSWD).decode().strip()
+    resp = requests.post(URL, headers=HEADERS,  data={'isTest': 'false', 'goformId': 'LOGIN', 'password': passwd})
+    if resp.status_code == 200:
+        output = resp.json()
+        if str(output['result']).upper() in ['0', 'OK', 'SUCCESS']:
+            print("Login succeeded")
+            return True
+    print("Login FAILED")
+    print(f"Response: {resp}")
+    print(f"Response: {resp.data}")
+    return False
+
+
+def send_sms(number, body):
+    try:
+        # max=765
+        body = encode_sms_body(body[:760], 'gsm7')
+        encoding = 'GSM7_default'
+    except UnicodeEncodeError:
+        # max=335
+        body = encode_sms_body(body[:330], 'utf16')
+        encoding = 'UNICODE'
+
+    print("[%s] => %s" % (encoding, body))
+
+    sms_time = time.strftime('%Y;%m;%d;%H;%M;%S;+2')[2:]
+
+    # encode_type=UNICODE | GSM7_default
+    # [UNICODE] łóść ŻÓL! => 0142 00F3 015B 0107 0020 017B 00D3 004C 0021
+    # [GSM7]
+    data = {'isTest': 'false',
+            'goformId': 'SEND_SMS',
+            'notCallback': 'true',
+            'Number': str(number),
+            'sms_time': sms_time,
+            'MessageBody': body,
+            'ID': '-1',
+            'encode_type': 'UNICODE'}
+    resp = requests.post(URL, headers=HEADERS, data=data)
+    print(resp.status_code)
+    print(resp.json())
+
+
+
+def check_received_sms(ts=None):
+    login()
+    if ts is None:
+        ts = str((time.time() - 3600) * 1000)
+    url = f'http://{ROUTER_IP}/goform/goform_get_cmd_process'
+    url_params = {
+        'isTest': 'false',
+        'cmd': 'sms_data_total',
+        'page': '0',
+        'data_per_page': '100',
+        'mem_store': '1',
+        'tags': '10',
+        'order_by': 'order by id desc',
+        '_': ts
+    }
+    raw_headers = f"""Host: 192.168.0.2
+User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/111.0
+Accept: application/json, text/javascript, */*; q=0.01
+Accept-Language: en-US,en;q=0.5
+Accept-Encoding: gzip, deflate
+X-Requested-With: XMLHttpRequest
+DNT: 1
+Connection: close
+Referer: http://{ROUTER_IP}/index.html"""
+    headers = dict([line.split(': ', 1) for line in raw_headers.splitlines()])
+    resp = requests.get(url, headers=headers, params=url_params)
+    data = resp.json()
+    for msg in data.get('messages'):
+        msg['content'] = decode_sms_body(msg['content'])
+        print(msg)
+        # logger.info("%6s %8s %s" % (msg['id'], msg['number'], decode_sms_body(msg['content'])))
+        # tag=2 -> wysłane
+        # tag=1 -> odebrane, nieprzeczytane
+        # tag=0 -> odebrane, przeczytane
+
+
+def delete_sms(msgid):
+    raw_request = f'''POST /goform/goform_set_cmd_process HTTP/1.1
+Host: 192.168.0.2
+User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/111.0
+Accept: application/json, text/javascript, */*; q=0.01
+Accept-Language: en-US,en;q=0.5
+Accept-Encoding: gzip, deflate
+Content-Type: application/x-www-form-urlencoded; charset=UTF-8
+X-Requested-With: XMLHttpRequest
+Content-Length: 63
+Origin: http://192.168.0.2
+DNT: 1
+Connection: keep-alive
+Referer: http://192.168.0.2/index.html
+'''
+    data = f'isTest=false&goformId=DELETE_SMS&msg_id={msgid}%3B&notCallback=true'
+
+
 def main():
+    if (len(sys.argv) in [2, 3]) and (sys.argv[1].strip('-').lower() == 'check'):
+        return check_received_sms(sys.argv[2] if len(sys.argv) == 3 else None)
+
     if len(sys.argv) != 3:
         hilfe()
 
@@ -179,50 +175,12 @@ def main():
         print("Invalid phone number: %s" % sys.argv[1])
         hilfe()
 
-    setup_router()
+    login()
 
-    print(f"Login {URL}")
+    number = sys.argv[1]
+    body = sys.argv[2]
+    send_sms(number, body)
 
-    passwd = base64.encodebytes(PASSWD).decode().strip()
-    resp = requests.post(URL, headers=HEADERS,  data={'isTest': 'false', 'goformId': 'LOGIN', 'password': passwd})
-    if resp.status_code == 200:
-        output = resp.json()
-        if str(output['result']).upper() in ['0', 'OK', 'SUCCESS']:
-
-            print("Login succeeded")
-            number = sys.argv[1]
-
-            body = sys.argv[2]
-
-            try:
-                # max=765
-                body = encode_sms_body(body[:760], 'gsm7')
-                encoding = 'GSM7_default'
-            except UnicodeEncodeError:
-                # max=335
-                body = encode_sms_body(body[:330], 'utf16')
-                encoding = 'UNICODE'
-
-            print("[%s] => %s" % (encoding, body))
-
-            sms_time = time.strftime('%Y;%m;%d;%H;%M;%S;+2')[2:]
-
-            # encode_type=UNICODE | GSM7_default
-            # [UNICODE] łóść ŻÓL! => 0142 00F3 015B 0107 0020 017B 00D3 004C 0021
-            # [GSM7]
-            data = {'isTest': 'false',
-                    'goformId': 'SEND_SMS',
-                    'notCallback': 'true',
-                    'Number': str(number),
-                    'sms_time': sms_time,
-                    'MessageBody': body,
-                    'ID': '-1',
-                    'encode_type': 'UNICODE'}
-            resp = requests.post(URL, headers=HEADERS, data=data)
-
-    print(resp.status_code)
-    print(resp.json())
 
 if __name__ == "__main__":
     main()
-
